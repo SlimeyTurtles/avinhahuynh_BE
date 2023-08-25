57c832ab56ef465f7f90570f08828078caceae92
Avinh Huynh <avinhahuynh@gmail.com>
2023-08-24 17:57:32 -0700

    set up for springboot maven

---
+++ src/main/java/com/avinh/avinhahuynh_be/AvinhahuynhBeApplication.java
@@ -0,0 +1,13 @@
+package com.avinh.avinhahuynh_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvinhahuynhBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvinhahuynhBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avinh/avinhahuynh_be/AvinhahuynhBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinh.avinhahuynh_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvinhahuynhBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
